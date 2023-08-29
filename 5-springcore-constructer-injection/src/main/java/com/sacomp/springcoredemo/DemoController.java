98018247c6d37a0ba3a99394ef4d94c88c2229fc
Salih Alcı <salihalci@gmail.com>
2023-08-29 21:18:56 +0300

    constructer injection project.

---
+++ 5-springcore-constructer-injection/src/main/java/com/sacomp/springcoredemo/DemoController.java
@@ -0,0 +1,27 @@
+package com.sacomp.springcoredemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    private Coach myCoach;
+
+    //tells spring to inject dependency
+    //if we have one constructer autowire keyword is optional.
+    @Autowired
+    public DemoController(Coach theCoach) {
+        this.myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String dailyWorkout(){
+
+
+        return myCoach.getDailyWorkout();
+
+
+    }
+}
