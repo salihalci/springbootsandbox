4550b96d891baaa8a5e8c49b99c2dec005575352
Salih Alcı <salihalci@gmail.com>
2023-08-29 16:48:38 +0300

    changed properties file. application context port etc

--- 4-commandline-demo/src/main/java/com/sa/springboot/myFirstApp/myfirstapp/rest/HelloController.java
+++ 4-commandline-demo/src/main/java/com/sa/springboot/myFirstApp/myfirstapp/rest/HelloController.java
@@ -6,8 +6,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
     @Value("${programmer.name}")
     private String programmerName;
+
     @GetMapping("/")
     public String sayHello(){
         return "Hello  :" +programmerName;
